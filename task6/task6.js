f9940e7f2bb128be93d52c072a0f4f64d63d9bd9
Premsagar Gedam <gedamprems@gmail.com>
2023-07-21 18:03:52 +0530

    Added task 6: accessed the json file | converted to js obj | updated the data | converted back to json | updated the json file

---
+++ task6/task6.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+const bookBuffer = fs.readFileSync('task6.json')
+
+const bookJSON = bookBuffer.toString()
+
+console.log("Book JSON : "+bookJSON)
+
+const bookObj = JSON.parse(bookJSON)
+console.log("Book OBJ : "+bookObj)
+
+bookObj.name = "Premsagar"
+bookObj.age = 25
+
+console.log("Book Updated OBJ : "+bookObj)
+
+const bookUpdatedJSON = JSON.stringify(bookObj)
+
+console.log("Book Updated JSON : "+bookUpdatedJSON)
+
+fs.writeFileSync('task6.json',bookUpdatedJSON)
