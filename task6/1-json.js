5cccdf37a4412e4fe4e001885c5338c80b30a88f
Premsagar Gedam <gedamprems@gmail.com>
2023-07-21 17:55:05 +0530

    Added task 6 example: json to js object | accessed the title

--- task6/1-json.js
+++ task6/1-json.js
@@ -2,13 +2,17 @@
 
 const fs = require('fs')
 
-const book = {
-    title: "My Title",
-    author: "Premsagar Gedam"
-}
+// const book = {
+//     title: "My Title",
+//     author: "Premsagar Gedam"
+// }
 
-const bookJSON = JSON.stringify(book)
-fs.writeFileSync('1-json.json',bookJSON)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json',bookJSON)
 
 const bookBuffer = fs.readFileSync('1-json.json')
 console.log(bookBuffer.toString())
+const bookJSON = bookBuffer.toString()
+const data = JSON.parse(bookJSON)
+const title = data.title
+console.log("Title: "+title)
