7ef47ea0e86a1d12c2456f9aa37dbc61fbf52a89
Premsagar Gedam <gedamprems@gmail.com>
2023-07-25 00:30:35 +0530

    Task Manager Project: Added add functionality

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 const fs = require('fs');
 const notes = require('./notes.js')
 
-console.log(notes.getNotes())
+const yargs = require('yargs')
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note!',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Body of Notes',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNotes(argv.title,argv.body)
+    }
+})
+
+
+
+// console.log(yargvs.argv) => 
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,6 +1,55 @@
-const addNotes = function(title, body){
+const fs = require('fs')
 
+
+// Function for adding notes
+
+const addNotes = function(title, body){
+    const notes = loadNotes()
+
+    let flag = false
+
+    for(let idx = 0; idx < notes.length; idx++){
+        if(notes[idx].title===title){
+            flag= true;
+            break;
+        }
+    }
+
+    if(flag){
+        // Printing title taken! 
+        console.log("Title taken!")
+    }else{
+        // Pushing data to the notes.json
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log("Note added!")
+    }  
 }
+
+// Helper function to load Notes 
+
+const loadNotes = function(){
+    try{
+        const notesBuffer = fs.readFileSync('./notes.json')
+        const notesJSON = notesBuffer.toString()
+        return JSON.parse(notesJSON)
+    }
+    catch(e){
+        return []
+    }
+}
+
+// Helper save updated notes to Notes.json
+
+const saveNotes = function(notes){
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('./notes.json',notesJSON)
+}
+
+// Function for getting Notes 
 
 const getNotes = function(){
     return "Your Notes..."
